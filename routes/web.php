ca94f5b47b6c5dcccd884e66473765917d0cb5de
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-12-11 01:07:35 -0300

    :sparkles: FEAT: ADDED MESSAGE MODEL AND MIGRATIONS

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/chat', [ChatController::class, 'index'])->name('chat');
+Route::get('/chat', [ChatController::class, 'index'])
+        ->name('chat')
+        ->middleware('auth');
 
 Route::redirect('/', '/chat');
 

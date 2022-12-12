3352c50dbe88fedffc37f38784ff5471353d72b3
João Paulo Fontana Macedo <61567948+Shaiqna@users.noreply.github.com>
2022-12-12 00:18:26 -0300

    :sparkles: FEAT: ADDED CONFIGURATION

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -34,3 +34,12 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+import Echo from 'laravel-echo';
+
+window.io = require('socket.io-client');
+
+window.Echo = new Echo({
+    broadcaster: 'socket.io',
+    host: window.location.hostname + ':6001'
+})
